f101785061784c2fee28d912811851dfd92156ef
Ashkree <maveron.tyriel@gmail.com>
2024-02-10 02:02:28 +0400

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
+// database
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch(() => {
+		console.log(err);
+	});
+
+// server
 const app = express();
 
 app.listen(3000, () => {
