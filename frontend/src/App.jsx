4eb165f6fc98b4188636fe90e95099b31d398b5b
Ashkree <maveron.tyriel@gmail.com>
2024-02-10 00:51:16 +0400

    Added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,32 @@
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SigUp";
+
 export default function App() {
-	return <div>App</div>;
+	return;
+	<BrowserRouter>
+		<Route
+			path="/"
+			element={<Home />}
+		/>
+		<Route
+			path="/about"
+			element={<About />}
+		/>
+		<Route
+			path="/profile"
+			element={<Profile />}
+		/>
+		<Route
+			path="/signin"
+			element={<SignIn />}
+		/>
+		<Route
+			path="/signout"
+			element={<SignUp />}
+		/>
+	</BrowserRouter>;
 }
