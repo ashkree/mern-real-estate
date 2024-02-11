04bec9cbd61eb07d0baede1b2938012c6d7b2475
Ashkree <maveron.tyriel@gmail.com>
2024-02-11 16:49:08 +0400

    created first user route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 // database
@@ -19,3 +20,6 @@ const app = express();
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
+
+// api routes
+app.use("/api/user", userRouter);
