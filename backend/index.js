c827af886c117f61b8f9c1aba02bed1d25df01d0
Ashkree <maveron.tyriel@gmail.com>
2024-02-11 21:35:26 +0400

    created error handler

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import { error } from "console";
 
 dotenv.config();
 
@@ -30,3 +31,15 @@ app.listen(3000, () => {
 // api routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// middleware for catching errors
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal Server Error";
+
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
+});
