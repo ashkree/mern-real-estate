2e2ddc8b97272f954fff445e589b29d3e33d8f45
Ashkree <maveron.tyriel@gmail.com>
2024-02-11 17:43:25 +0400

    created signup auth

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 // database
@@ -17,9 +19,14 @@ mongoose
 // server
 const app = express();
 
+// json to be sent to the server
+app.use(express.json());
+
+// listen on port 3000 and print if serving is running on 300
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
 
 // api routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
